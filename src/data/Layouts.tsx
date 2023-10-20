c4773c647705950ff2bb67dd60a57215edf5fcea
Ron Yifeng Wang <yifengwang@berkeley.edu>
2023-10-20 16:49:56 -0700

    layout demo

--- src/data/Layouts.tsx
+++ src/data/Layouts.tsx
@@ -5,6 +5,7 @@ export interface LayoutType {
 
 export const layouts: LayoutType[] = [
   {
+    //
     board:
     [
       '131242',
@@ -34,4 +35,33 @@ export const layouts: LayoutType[] = [
     ],
     elimNum: 3
   },
+
+  {
+    board:
+    [
+      '2414151', 
+      '4245233',
+      '2245234',
+    ],
+    elimNum: 3
+  },
+
+  // eliminate green first or red first
+  {
+    board:
+    ['333341', '232412', '412432', '321442', '421213', '414311'],
+    elimNum: 3
+  },
+
+    // eliminate red first or green first
+    {
+      board:
+      ['333341', '414311', '232412', '412432', '321442', '421213'],
+      elimNum: 3
+    },
+  
+
+  
+  
+
 ]
